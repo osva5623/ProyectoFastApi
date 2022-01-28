ca3dd44ab9f8b8c3ec6588841366229274cfd1b2
Oswaldo Jimenez <oswaldo.jimenez5623@gmail.com>
2022-01-28 14:25:15 -0600

    segunda irl

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+#python
+from typing import Optional
+#pydantic
+from pydantic import BaseModel
+#Fastapi
 from fastapi import FastAPI
+from fastapi import Body
 
 app=FastAPI()
+
+#models
+
+class Person(BaseModel):
+    first_name:str
+    last_name:str
+    age:int
+    head_color:Optional[int]=None
+    is_married:Optional[bool]=None
 
 @app.get("/")
 def home():
     return {"hello":"world"}
+
+@app.get("/person/new")
+def create_person(person:Person=Body(...)):
+    return person
